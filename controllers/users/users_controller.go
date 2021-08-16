0d9fa2aac08ead8a81744be992f8e1028bf41340
Kartik <kartik@hyperverge.co>
2021-08-17 00:26:49 +0530

    Update folder structure

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,19 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(ctx *gin.Context) {
+	ctx.String(http.StatusNotImplemented, "Not Implemented")
+}
+
+func GetUser(ctx *gin.Context) {
+	ctx.String(http.StatusNotImplemented, "Not Implemented")
+}
+
+func SearchUser(ctx *gin.Context) {
+	ctx.String(http.StatusNotImplemented, "Not Implemented")
+}
