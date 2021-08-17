16e1ca8ab1140f147b6d1d59954c7afcc2d79d86
Kartik <kartik@hyperverge.co>
2021-08-17 19:09:37 +0530

    Add: basic user controller

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,37 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kartik1998/bookstore-users-api/domain/users"
+	"github.com/kartik1998/bookstore-users-api/services"
 )
 
 func CreateUser(ctx *gin.Context) {
-	ctx.String(http.StatusNotImplemented, "Not Implemented")
+	var user users.User
+	bytes, errR := ioutil.ReadAll(ctx.Request.Body)
+	if errR != nil {
+		// handle err
+		fmt.Println("err ", errR)
+		return
+	}
+	errP := json.Unmarshal(bytes, &user)
+	if errP != nil {
+		// handle err
+		fmt.Println("errP ", errP)
+		return
+	}
+	result, errS := services.CreateUser(user)
+	if errS != nil {
+		// handle user creation err
+		return
+	}
+	fmt.Println("result ", result)
+	ctx.JSON(http.StatusCreated, result)
 }
 
 func GetUser(ctx *gin.Context) {
--- services/user_service.go
+++ services/user_service.go
@@ -1,1 +1,7 @@
 package services
+
+import "github.com/kartik1998/bookstore-users-api/domain/users"
+
+func CreateUser(user users.User) (*users.User, error) {
+	return &user, nil
+}
